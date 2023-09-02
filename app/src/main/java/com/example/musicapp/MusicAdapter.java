2aa8d1c11b025a63b019e71d0fb8b4807005e884
Le Minh Vu <leminhvu0201@gmail.com>
2023-09-02 09:41:01 +0700

    create music app

---
+++ app/src/main/java/com/example/musicapp/MusicAdapter.java
@@ -0,0 +1,95 @@
+//Apdapter thêm vào view list
+
+package com.example.musicapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MusicAdapter extends BaseAdapter implements Filterable {
+    private Context context;
+    private int layout;
+    private ArrayList<Music>arrayMusic;
+    private ArrayList<Music>arrayMusicOld;
+
+    public MusicAdapter(Context context, int layout, ArrayList<Music> arrayMusic) {
+        this.context = context;
+        this.layout = layout;
+        this.arrayMusic = arrayMusic;
+        this.arrayMusicOld = arrayMusic;
+    }
+
+    @Override
+    public int getCount() {
+        return arrayMusic.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        view = inflater.inflate(layout,null);
+
+        TextView txtMusicName = (TextView) view.findViewById(R.id.musicName);
+        TextView txtMusicianName = (TextView) view.findViewById(R.id.musicianName);
+        ImageView musicImage = (ImageView) view.findViewById(R.id.musicImage);
+
+        Music music = arrayMusic.get(i);
+
+        txtMusicName.setText(music.getTenNhac());
+        txtMusicianName.setText(music.getTacGia());
+        musicImage.setImageResource(music.getHinhNen());
+
+        return view;
+    }
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                String strSearch = charSequence.toString();
+                if(strSearch.isEmpty()){
+                    arrayMusic = arrayMusicOld;
+                }
+                else{
+                    ArrayList<Music> arraySearch = new ArrayList<>();
+                    for(Music music : arrayMusic){
+                        if(music.getTenNhac().toLowerCase().contains(strSearch.toLowerCase())){
+                            arraySearch.add(music);
+                        }
+                    }
+                    arrayMusic = arraySearch;
+                }
+                MainActivity.setArrayMusic(arrayMusic);
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = arrayMusic;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                arrayMusic = (ArrayList<Music>)filterResults.values;
+                notifyDataSetChanged();
+            }
+        };
+    }
+}
---
+++ app/src/main/java/com/example/musicapp/PlayMusicActivity.java
@@ -0,0 +1,197 @@
+package com.example.musicapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+public class PlayMusicActivity extends AppCompatActivity {
+
+    private TextView musicName, runTime, totalTime;
+    private ImageView musicImage;
+    private SeekBar seekBar;
+    private ImageButton playPauseButton,preButton,nextButton;
+    private MediaPlayer mediaPlayer;
+    private Animation animation;
+    private ArrayList<Music> arrayMusic;
+    private int position;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_music);
+        setAnimation();
+        AnhXa();
+        getData();
+        setMusicPlayImage();
+        setMusicName();
+        khoiTaoMediaPlayer();
+        setTimeTotal();
+        playPauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mediaPlayer.isPlaying()){
+                    mediaPlayer.pause();
+                    animation.cancel();
+                    musicImage.setAnimation(null);
+                    playPauseButton.setImageResource(R.drawable.play);
+                }
+                else{
+                    mediaPlayer.start();
+                    musicImage.startAnimation(animation);
+                    playPauseButton.setImageResource(R.drawable.pause);
+                }
+                updateRunTime();
+            }
+        });
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                position++;
+                if (position >= arrayMusic.size()){
+                    position = 0;
+                }
+
+                if(mediaPlayer.isPlaying()){
+                    mediaPlayer.stop();
+                }
+                khoiTaoMediaPlayer();
+                setMusicPlayImage();
+                setMusicName();
+                setTimeTotal();
+                updateRunTime();
+                musicImage.startAnimation(animation);
+                playPauseButton.setImageResource(R.drawable.pause);
+                mediaPlayer.start();
+            }
+        });
+        preButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                position--;
+                if(position < 0){
+                    position = arrayMusic.size()-1;
+                }
+
+                if ((mediaPlayer.isPlaying())){
+                    mediaPlayer.stop();
+                }
+                khoiTaoMediaPlayer();
+                setMusicPlayImage();
+                setMusicName();
+                setTimeTotal();
+                updateRunTime();
+                musicImage.startAnimation(animation);
+                playPauseButton.setImageResource(R.drawable.pause);
+                mediaPlayer.start();
+            }
+        });
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                mediaPlayer.seekTo(seekBar.getProgress());
+                if(!mediaPlayer.isPlaying()){
+                    playPauseButton.setImageResource(R.drawable.pause);
+                    updateRunTime();
+                    mediaPlayer.start();
+                }
+            }
+        });
+    }
+
+    private void setAnimation() {
+        animation = AnimationUtils.loadAnimation(this,R.anim.rotate);
+    }
+
+    private void updateRunTime(){
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                SimpleDateFormat running = new SimpleDateFormat("mm:ss");
+                runTime.setText(running.format(mediaPlayer.getCurrentPosition()));
+                seekBar.setProgress(mediaPlayer.getCurrentPosition());
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mp) {
+                        position++;
+                        if (position >= arrayMusic.size()){
+                            position = 0;
+                        }
+
+                        if(mediaPlayer.isPlaying()){
+                            mediaPlayer.stop();
+                        }
+                        setMusicPlayImage();
+                        setMusicName();
+                        khoiTaoMediaPlayer();
+                        setTimeTotal();
+                        updateRunTime();
+                        playPauseButton.setImageResource(R.drawable.pause);
+                        mediaPlayer.start();
+                    }
+                });
+                handler.postDelayed(this,500);
+            }
+        },100);
+    }
+    private void setTimeTotal(){
+        SimpleDateFormat total = new SimpleDateFormat("mm:ss");
+        totalTime.setText(total.format(mediaPlayer.getDuration()));
+        seekBar.setMax(mediaPlayer.getDuration());
+    }
+    private void khoiTaoMediaPlayer(){
+        mediaPlayer = MediaPlayer.create(PlayMusicActivity.this, arrayMusic.get(position).getFile());
+    }
+    private void setMusicName() {
+        musicName.setText(arrayMusic.get(position).getTenNhac());
+    }
+
+    private void setMusicPlayImage() {
+        musicImage.setImageResource(arrayMusic.get(position).getHinhNen());
+    }
+
+    private void getData() {
+        arrayMusic = MainActivity.getArrayMusic();
+        Intent it = getIntent();
+        position = Integer.parseInt(it.getStringExtra("position"));
+    }
+    private void AnhXa() {
+        musicName = (TextView) findViewById(R.id.headMusicName);
+        runTime= (TextView) findViewById(R.id.runTime);
+        totalTime = (TextView) findViewById(R.id.totalTime);
+        musicImage = (ImageView) findViewById(R.id.musicPlayImage);
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        playPauseButton = (ImageButton) findViewById(R.id.playPauseButton);
+        preButton = (ImageButton) findViewById(R.id.preButton);
+        nextButton = (ImageButton) findViewById(R.id.nextButton);
+    }
+    @Override
+    public void onBackPressed() {
+        // Dừng phát nhạc khi người dùng bấm nút "Back"
+        if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+        }
+        super.onBackPressed();
+    }
+}
