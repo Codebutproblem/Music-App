5a55cbb0a386387e8f62e85da6b4a148eda1452d
Le Minh Vu <leminhvu0201@gmail.com>
2023-09-02 09:41:01 +0700

    create music app

---
+++ app/src/main/java/com/example/musicapp/Music.java
@@ -0,0 +1,40 @@
+package com.example.musicapp;
+
+
+
+//Du lieu am nhac
+// Tao class
+
+public class Music {
+    private String tenNhac; //Ten nhac
+    private String tacGia; //Ten tac gia
+    private int hinhNen; //file hinh anh am nhac
+    private int file; //file am thanh
+
+    //Constructor
+    public Music(String tenNhac, String tacGia, int hinhNen, int file) {
+        this.tenNhac = tenNhac;
+        this.tacGia = tacGia;
+        this.hinhNen = hinhNen;
+        this.file = file;
+    }
+
+
+
+    //Getter
+    public int getFile() {
+        return file;
+    }
+
+    public String getTenNhac() {
+        return tenNhac;
+    }
+
+    public String getTacGia() {
+        return tacGia;
+    }
+
+    public int getHinhNen() {
+        return hinhNen;
+    }
+}
